8c6f3f0a151761790a6aa5f0b2717548c65876ab
PylaevAleksandr <pylaev.aleksandr.99@mail.ru>
2025-06-07 16:40:15 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class SquareMatrix
+{
+    private double[,] matrix;
+    private int size;
+
+                                                            // Конструктор случайной матрицы
+    public SquareMatrix(int size)
+    {
+        this.size = size;
+        matrix = new double[size, size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                matrix[i, j] = rand.NextDouble() * 10;
+            }
+        }
+    }
+}                                                          // Сделал не с первого раза, просто забыл репнуть его, и сохранилась только готовая персия
+
+
